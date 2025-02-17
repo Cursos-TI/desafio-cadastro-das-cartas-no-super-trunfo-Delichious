e912b483f94a42c0a32f72eda01b021d12ad99a8
Delichious <86540746+Delichious@users.noreply.github.com>
2025-02-17 15:27:56 +0000

    criei o estado de são paulo.

--- MeuSuperTrunfo.c
+++ MeuSuperTrunfo.c
@@ -3,9 +3,11 @@
 int main(){
     printf("Bem vindos todos a meu começo de um jogo de super trunfo, agradeço de antemão por jogarem!\n");
 
-    int país;
-    printf("Digite seu país: ");
-    scanf("%s", &país);
-    printf("Seu país é: %s\n", país = Brasil)
+    printf("O primeiro país,o Brasil! Com sua vasta vegetação e variedades de biomas, também é um grande mercador, e um oponente forte contra os demais países.")
+
+    char nome[5] = "Brasil";
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado);
 
 } 
