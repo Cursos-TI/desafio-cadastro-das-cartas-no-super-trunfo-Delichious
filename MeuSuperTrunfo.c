80b46e3b7399d62f7126ce7ca964cced93652e74
Delichious <86540746+Delichious@users.noreply.github.com>
2025-02-17 14:04:33 +0000

    criei o primeiro país.

---
+++ MeuSuperTrunfo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("Bem vindos todos a meu começo de um jogo de super trunfo, agradeço de antemão por jogarem!\n");
+
+    int país;
+    printf("Digite seu país: ");
+    scanf("%s", &país);
+    printf("Seu país é: %s\n", país = Brasil)
+
+} 
