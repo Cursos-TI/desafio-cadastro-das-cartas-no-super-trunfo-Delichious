3439d288c806960d3dfd8200eebe31777aa055a3
Delichious <86540746+Delichious@users.noreply.github.com>
2025-02-18 14:13:05 +0000

    criei mais 7 estados

--- MeuSuperTrunfo.c
+++ MeuSuperTrunfo.c
@@ -3,11 +3,42 @@
 int main(){
     printf("Bem vindos todos a meu começo de um jogo de super trunfo, agradeço de antemão por jogarem!\n");
 
-    printf("O primeiro país,o Brasil! Com sua vasta vegetação e variedades de biomas, também é um grande mercador, e um oponente forte contra os demais países.")
+    printf("O primeiro país,o Brasil! Com sua vasta vegetação e variedades de biomas, também é um grande mercador, e um oponente forte contra os demais países.");
 
-    char nome[5] = "Brasil";
+    char nome[7] = "Brasil";
+
+    char nome[7] = "estado";
+
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n","(estado [11])('São Paulo')'";
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado = "Rio de Janeiro");
     printf(" Digite seu estado: ");
     scanf("%s", &estado);
     printf("Seu estado é: %s\n", estado);
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado = "Mato Grosso");
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado = "Amazonas");
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado = "Ceará");
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado = "Tocantins");
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado = "Curitiba");
+    printf(" Digite seu estado: ");
+    scanf("%s", &estado);
+    printf("Seu estado é: %s\n", estado = "Paraná");
+    return 0;
+
+    char nome[20] = "Bolívia";
+
 
 } 
